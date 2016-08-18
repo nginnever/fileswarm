86d1eeb02edf0b7726e0ba638aee8e95db1e4386
nginnever <ginneversource@gmail.com>
2016-08-18 16:53:05 -0700

    begin photon layout osx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,38 @@ export const Main = React.createClass({
   render: function() {
     return (
       <div className="window">
+      <header className="toolbar toolbar-header">
+  <h1 className="title">Header with actions</h1>
+
+  <div className="toolbar-actions">
+    <div className="btn-group">
+      <button className="btn btn-default">
+        <span className="icon icon-home"></span>
+      </button>
+      <button className="btn btn-default">
+        <span className="icon icon-folder"></span>
+      </button>
+      <button className="btn btn-default active">
+        <span className="icon icon-cloud"></span>
+      </button>
+      <button className="btn btn-default">
+        <span className="icon icon-popup"></span>
+      </button>
+      <button className="btn btn-default">
+        <span className="icon icon-shuffle"></span>
+      </button>
+    </div>
+
+    <button className="btn btn-default">
+      <span className="icon icon-home icon-text"></span>
+      Filters
+    </button>
+
+    <button className="btn btn-default btn-dropdown pull-right">
+      <span className="icon icon-megaphone"></span>
+    </button>
+  </div>
+</header>
         <div className="window-content">
           <div className="pane-group">
             <div className="pane-sm sidebar">
@@ -17,6 +49,17 @@ export const Main = React.createClass({
             </div>
           </div>
         </div>
+     <footer className="toolbar toolbar-footer">
+  <div className="toolbar-actions">
+    <button className="btn btn-default">
+      Cancel
+    </button>
+
+    <button className="btn btn-primary pull-right">
+      Save
+    </button>
+  </div>
+</footer>
       </div>
     )
   }
